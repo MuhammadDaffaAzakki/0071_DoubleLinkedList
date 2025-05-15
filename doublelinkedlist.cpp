f01e0dbc867d4fc3a22bebe09921f9ba18ccb0cf
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-05-15 19:26:56 +0700

    menambahkan step 3 dan 4 didalam void hapus

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -111,4 +111,13 @@ public:
             START = current->next; // Step 2a: START = START.next
             if (START != NULL)
                 START->prev = NULL; // Step 2b: START.prev = NULL
+        }
+        else
+        {
+            // Step 3: Link previous node to next of current
+            current->prev->next = current->next;
 
+            // Step 4: if current is not the last node
+            if (current->next != NULL)
+                current->next->prev = current->prev;
+        }
