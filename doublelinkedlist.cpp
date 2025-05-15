939ade95ddd58183a3439a376800591e9fa37904
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-05-15 19:08:07 +0700

    menambahkan step 9

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -69,3 +69,14 @@ public:
             cout << "\nDuplicate roll numbers not allowed" << endl;
             return;
         }
+
+        // Step 9: insert between current and current->next
+        newNode->next = current->next; // Step 9a: newNode.next = current->next
+        newNode->prev = current;     // Step 9b: newNode.prev = current
+
+        // insert last node
+        if (current->next != NULL)
+            current->next->prev = newNode; // Step 9c
+
+        current->next = newNode; // Step 9d
+    }
