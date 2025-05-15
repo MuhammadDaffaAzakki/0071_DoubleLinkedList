a3986fb3cacbdd1404ad0881d091c0bd1421b38b
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-05-15 18:47:29 +0700

    membuat class doublelinkedlist

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -4,8 +4,19 @@ using namespace std;
 
 class Node
 {
-    public:
-        int noMhs;
-        Node*next;
-        Node *prev;
+public:
+    int noMhs;
+    Node *next;
+    Node *prev;
 };
+
+class DoubleLinkedList
+{
+private:
+    Node *START;
+
+public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
